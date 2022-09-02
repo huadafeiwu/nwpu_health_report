35f8ff4e9632c4366c53390d27f5ef0b890560ac
huadafeiwu <78134295+huadafeiwu@users.noreply.github.com>
2022-09-02 23:24:45 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -35,7 +35,7 @@ time.sleep(1)
 browser.find_element(By.LINK_TEXT, "提交填报信息").click() # 提交填报信息
 browser.find_element(By.XPATH, "//label[@class='weui-cell weui-cell_active weui-check__label']").click() # 已核实 
 #browser.find_element(By.XPATH, "//i[@class='weui-icon-checked']").click() # 已核实 
-#browser.find_element(By.ID, "save_div").click() # 确认提交
-#time.sleep(1)
+browser.find_element(By.ID, "save_div").click() # 确认提交
+time.sleep(1)
     
 browser.quit()
