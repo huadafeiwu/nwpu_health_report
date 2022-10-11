4368100ae009aacc354a2832379ac1a77c449e98
huadafeiwu <78134295+huadafeiwu@users.noreply.github.com>
2022-10-11 16:13:30 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -23,7 +23,7 @@ username = browser.find_element(By.ID, 'username')
 username.send_keys(USERNAME)
 password = browser.find_element(By.ID, 'password')
 password.send_keys(PASSWORD)
-addr = browser.find_element(By.CLASS_NAME, 'el-button el-button--primary el-button--medium is-round')
+browser.find_element(By.XPATH, "//input[@class='el-button el-button--primary el-button--medium is-round']").click()
 time.sleep(1)
 
 browser.quit()
