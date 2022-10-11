9b1f954472e1ebff018185cf46fc51e6d5d1f1b2
huadafeiwu <78134295+huadafeiwu@users.noreply.github.com>
2022-10-11 16:27:16 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -17,12 +17,14 @@ browser = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_option
 # open nwpu
 browser.get('https://uis.nwpu.edu.cn/cas/login?service=https://ecampus.nwpu.edu.cn/')
 browser.maximize_window()
+time.sleep(5)
 
 # login
 username = browser.find_element(By.ID, 'username')
 username.send_keys(USERNAME)
 password = browser.find_element(By.ID, 'password')
 password.send_keys(PASSWORD)
+
 aabb = browser.find_element(By.NAME, "execution")
 time.sleep(1)
 
