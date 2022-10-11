8e04bf7d9b328a18361143867d06b80f49b10970
huadafeiwu <78134295+huadafeiwu@users.noreply.github.com>
2022-10-11 16:18:10 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -23,7 +23,7 @@ username = browser.find_element(By.ID, 'username')
 username.send_keys(USERNAME)
 password = browser.find_element(By.ID, 'password')
 password.send_keys(PASSWORD)
-browser.find_element(By.XPATH, "//div[@class='el-form-item__content']").click()
+browser.find_element(By.NAME, "execution").click()
 time.sleep(1)
 
 browser.quit()
