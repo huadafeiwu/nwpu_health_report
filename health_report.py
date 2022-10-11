4f4a92a866bf156da43260171b47f4f60acd1228
huadafeiwu <78134295+huadafeiwu@users.noreply.github.com>
2022-10-11 15:58:09 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -14,28 +14,6 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 browser = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
 
-# open nwpu
-browser.get('https://uis.nwpu.edu.cn/cas/login?service=https://ecampus.nwpu.edu.cn/')
-browser.maximize_window()
 
-# login
-username = browser.find_element(By.ID, "username")
-username.send_keys(USERNAME)
-password = browser.find_element(By.ID, "password")
-password.send_keys(PASSWORD)
-browser.find_element(By.NAME, "submit").click()
-time.sleep(1)
 
-#browser.get('https://yqtb.nwpu.edu.cn/wx/xg/yz-mobile/index.jsp')
-#browser.refresh()
-browser.get('https://yqtb.nwpu.edu.cn/wx/ry/jrsb_xs.jsp')
-browser.refresh()
-time.sleep(1)
-
-browser.find_element(By.LINK_TEXT, "提交填报信息").click() # 提交填报信息
-browser.find_element(By.XPATH, "//label[@class='weui-cell weui-cell_active weui-check__label']").click() # 已核实 
-#browser.find_element(By.XPATH, "//i[@class='weui-icon-checked']").click() # 已核实 
-browser.find_element(By.ID, "save_div").click() # 确认提交
-time.sleep(1)
-    
 browser.quit()
